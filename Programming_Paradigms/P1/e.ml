c2f2b4579f4c04619604e1f4cd163b74935b828c
Pablo Manzanares López <135281231+peybol45@users.noreply.github.com>
2023-10-03 13:12:27 +0200

    Add files via upload

--- Programming_Paradigms/P1/e.ml
+++ Programming_Paradigms/P1/e.ml
@@ -1,3 +1,5 @@
-let rec factorial n = if n > 1. then n *. factorial (n -. 1.) else 1. in 
+(*let rec factorial n = if n > 1. then n *. factorial (n -. 1.) else 1. in 
 let rec calc_e i = if i < 50. then 1. /. factorial i +. calc_e (i +. 1.) else 0. in 
-Printf.printf "%.15f\n" (calc_e 0.)
+Printf.printf "%.15f\n" (calc_e 0.)*)
+
+Printf.printf "%.15f\n" (exp 1.)
