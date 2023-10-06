1db079aba68d55b9ec6d3d8e62d0e4fe54b7ce06
Pablo Manzanares López <135281231+peybol45@users.noreply.github.com>
2023-10-06 17:47:53 +0200

    Delete Programming_Paradigms/P2 directory

--- Programming_Paradigms/P2/def.ml
+++
@@ -1,9 +0,0 @@
-let v = (function x -> 2 * x + 1) 5;;
-
-let w = 4. *. 3. +. 5. -. 8. /. 2.;;
-
-let x = if (4 < 3) then 'x' else 'd';;
-
-let y = (function true -> false | false -> true) (5+3 > 8);;
-
-let z = "Hola Mund" ^ (String.make 1 (Char.chr (11 + int_of_char 'd')));;
