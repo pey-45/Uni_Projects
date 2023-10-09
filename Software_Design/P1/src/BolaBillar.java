be2d2720cbcd9ee7a758c9635c8fbf5a413c7a5b
Pablo Manzanares López <135281231+peybol45@users.noreply.github.com>
2023-10-09 13:31:48 +0200

    Add files via upload

--- Software_Design/P1/src/BolaBillar.java
+++ Software_Design/P1/src/BolaBillar.java
@@ -1,49 +1,35 @@
 package ej4;
 
-public class BolaBillar
+public enum BolaBillar
 {
-    private enum Bola
+    BLANCA (0, "BLANCO", "LISA"),
+    BOLA1 (1, "AMARILLO", "LISA"),
+    BOLA2 (2, "AZUL", "LISA"),
+    BOLA3 (3, "ROJO", "LISA"),
+    BOLA4 (4, "VIOLETA", "LISA"),
+    BOLA5 (5, "NARANJA", "LISA"),
+    BOLA6 (6, "VERDE", "LISA"),
+    BOLA7 (7, "GRANATE", "LISA"),
+    BOLA8 (8, "NEGRO", "LISA"),
+    BOLA9 (9, "AMARILLO", "RAYADA"),
+    BOLA10 (10, "AZUL", "RAYADA"),
+    BOLA11 (11, "ROJO", "RAYADA"),
+    BOLA12 (12, "VIOLETA", "RAYADA"),
+    BOLA13 (13, "NARANJA", "RAYADA"),
+    BOLA14 (14, "VERDE", "RAYADA"),
+    BOLA15 (15, "GRANATE", "RAYADA");
+
+    private final int num;
+    private final String color, type;
+
+    public int getNum() {return num;}
+    public String getColor() {return color;}
+    public String getType() {return type;}
+
+    BolaBillar(int num, String color, String type)
     {
-        BLANCA (0, "BLANCO", "LISA"),
-        BOLA1 (1, "AMARILLO", "LISA"),
-        BOLA2 (2, "AZUL", "LISA"),
-        BOLA3 (3, "ROJO", "LISA"),
-        BOLA4 (4, "VIOLETA", "LISA"),
-        BOLA5 (5, "NARANJA", "LISA"),
-        BOLA6 (6, "VERDE", "LISA"),
-        BOLA7 (7, "GRANATE", "LISA"),
-        BOLA8 (8, "NEGRO", "LISA"),
-        BOLA9 (9, "AMARILLO", "RAYADA"),
-        BOLA10 (10, "AZUL", "RAYADA"),
-        BOLA11 (11, "ROJO", "RAYADA"),
-        BOLA12 (12, "VIOLETA", "RAYADA"),
-        BOLA13 (13, "NARANJA", "RAYADA"),
-        BOLA14 (14, "VERDE", "RAYADA"),
-        BOLA15 (15, "GRANATE", "RAYADA");
-
-        private final int num;
-        private final String color;
-        private final String type;
-
-        Bola (int num, String color, String type)
-        {
-            this.num = num;
-            this.color = color;
-            this.type = type;
-        }
-
-        public int getNum() { return this.num; }
-        public String getColor() { return this.color; }
-        public String getType() { return this.type; }
-    }
-
-    private Bola bola;
-    public BolaBillar(int n)
-    {
-        for (Bola bola : Bola.values())
-        {
-            if (n==bola.getNum())
-                this.bola = bola;
-        }
+        this.num = num;
+        this.color = color;
+        this.type = type;
     }
 }
--- Software_Design/P1/src/MesaBillar.java
+++ Software_Design/P1/src/MesaBillar.java
@@ -1,17 +1,68 @@
 package ej4;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MesaBillar
 {
-    private ArrayList<BolaBillar> bolas;
-    int i;
+    private final ArrayList<BolaBillar> ballsOverTable = new ArrayList<>();
+    private final ArrayList<BolaBillar> ballsInBox = new ArrayList<>();
+    private boolean partidaIniciada = false;
+
 
     public MesaBillar()
     {
-        for (i = 0; i < 16; i++)
+        ballsInBox.addAll(Arrays.asList(BolaBillar.values()));
+    }
+
+    public void iniciarPartida()
+    {
+        ballsInBox.clear();
+        ballsOverTable.addAll(Arrays.asList(BolaBillar.values()));
+        partidaIniciada = true;
+    }
+
+    public void meterBola(BolaBillar bola)
+    {
+        if (!partidaIniciada)
+            throw new IllegalArgumentException();
+
+        if (bola.getColor().equals("BLANCO"))
+            return;
+
+        if (bola.getColor().equals("NEGRO"))
+            partidaIniciada = false;
+        else
         {
-            bolas.add(new BolaBillar(i));
+            ballsOverTable.remove(bola);
+            ballsInBox.add(bola);
         }
+    }
+
+    public ArrayList<BolaBillar> bolasMesa() { return ballsOverTable; }
+
+    public ArrayList<BolaBillar> bolasCajetin() { return ballsInBox; }
+
+    public boolean isPartidaIniciada() { return partidaIniciada; }
+
+    public String obtenerGanador()
+    {
+        int i, n_rayadas = isPartidaIniciada()? 0:2, n_lisas = 0;
+
+        for (i = 0; i < ballsInBox.size(); i++)
+        {
+            if (ballsInBox.get(i).getColor().equals("WHITE") || ballsInBox.get(i).getColor().equals("BLACK"))
+                continue;
+
+            if (ballsInBox.get(i).getType().equals("RAYADA"))
+                n_rayadas++;
+            else
+                n_lisas++;
+        }
+
+        if (n_rayadas==n_lisas)
+            return "EMPATE";
+
+        return n_rayadas > n_lisas? "RAYADAS":"LISAS";
     }
 }
